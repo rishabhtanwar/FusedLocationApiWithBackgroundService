35bd37590f4e43a5bd16f7f1d228ff6df1aae674
Tiit Hallas <tiitha@scada.local>
2018-08-08 21:52:42 +0300

    Initial commit

---
+++ app/src/main/java/com/geoape/backgroundlocationexample/BackgroundService.java
@@ -0,0 +1,129 @@
+package com.geoape.backgroundlocationexample;
+
+import android.app.Notification;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Binder;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class BackgroundService extends Service {
+    private final LocationServiceBinder binder = new LocationServiceBinder();
+    private final String TAG = "BackgroundService";
+    private LocationListener mLocationListener;
+    private LocationManager mLocationManager;
+    private final int LOCATION_INTERVAL = 500;
+    private final int LOCATION_DISTANCE = 10;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    private class LocationListener implements android.location.LocationListener
+    {
+        private Location lastLocation = null;
+        private final String TAG = "LocationListener";
+        private Location mLastLocation;
+
+        public LocationListener(String provider)
+        {
+            mLastLocation = new Location(provider);
+        }
+
+        @Override
+        public void onLocationChanged(Location location)
+        {
+            mLastLocation = location;
+            Log.i(TAG, "LocationChanged: "+location);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider)
+        {
+            Log.e(TAG, "onProviderDisabled: " + provider);
+        }
+
+        @Override
+        public void onProviderEnabled(String provider)
+        {
+            Log.e(TAG, "onProviderEnabled: " + provider);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras)
+        {
+            Log.e(TAG, "onStatusChanged: " + status);
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        super.onStartCommand(intent, flags, startId);
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onCreate()
+    {
+        Log.i(TAG, "onCreate");
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+        if (mLocationManager != null) {
+            try {
+                mLocationManager.removeUpdates(mLocationListener);
+            } catch (Exception ex) {
+                Log.i(TAG, "fail to remove location listners, ignore", ex);
+            }
+        }
+    }
+
+    private void initializeLocationManager() {
+        if (mLocationManager == null) {
+            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
+    public void startTracking() {
+        initializeLocationManager();
+        mLocationListener = new LocationListener(LocationManager.GPS_PROVIDER);
+
+        try {
+            mLocationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE, mLocationListener );
+
+        } catch (java.lang.SecurityException ex) {
+            // Log.i(TAG, "fail to request location update, ignore", ex);
+        } catch (IllegalArgumentException ex) {
+            // Log.d(TAG, "gps provider does not exist " + ex.getMessage());
+        }
+
+    }
+
+    public void stopTracking() {
+        this.onDestroy();
+    }
+
+
+
+
+    public class LocationServiceBinder extends Binder {
+        public BackgroundService getService() {
+            return BackgroundService.this;
+        }
+    }
+
+}
