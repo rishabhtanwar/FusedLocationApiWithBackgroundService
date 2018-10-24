b08a9855475333b8504569ec5129f4cc43741233
rishabh <tanwar.rishabh0@gmail.com>
2018-10-24 17:10:25 +0530

    first commit

--- app/src/main/java/com/geoape/backgroundlocationexample/BackgroundService.java
+++ app/src/main/java/com/geoape/backgroundlocationexample/BackgroundService.java
@@ -4,6 +4,7 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
@@ -12,132 +13,117 @@ import android.os.Binder;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import java.util.ArrayList;
 import java.util.Date;
 
 public class BackgroundService extends Service {
-    private final LocationServiceBinder binder = new LocationServiceBinder();
-    private final String TAG = "BackgroundService";
-    private LocationListener mLocationListener;
-    private LocationManager mLocationManager;
-    private NotificationManager notificationManager;
+  private final LocationServiceBinder binder = new LocationServiceBinder();
+  private final String TAG = "BackgroundService";
+  private LocationListener mLocationListener;
+  private GoogleApiClient mGoogleApiClient;
+  private LocationRequest locationRequest;
+  LocationListener connectionCallbacks;
+  LocationListener connectionFailedListener;
+  private final int LOCATION_INTERVAL = 0;
 
-    private final int LOCATION_INTERVAL = 500;
-    private final int LOCATION_DISTANCE = 10;
+  @Override public IBinder onBind(Intent intent) {
+    return binder;
+  }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return binder;
+  private class LocationListener implements com.google.android.gms.location.LocationListener,
+      GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private final String TAG = "LocationListener";
+
+    public LocationListener() {
     }
 
-    private class LocationListener implements android.location.LocationListener
-    {
-        private Location lastLocation = null;
-        private final String TAG = "LocationListener";
-        private Location mLastLocation;
-
-        public LocationListener(String provider)
-        {
-            mLastLocation = new Location(provider);
-        }
-
-        @Override
-        public void onLocationChanged(Location location)
-        {
-            mLastLocation = location;
-            Log.i(TAG, "LocationChanged: "+location);
-        }
-
-        @Override
-        public void onProviderDisabled(String provider)
-        {
-            Log.e(TAG, "onProviderDisabled: " + provider);
-        }
-
-        @Override
-        public void onProviderEnabled(String provider)
-        {
-            Log.e(TAG, "onProviderEnabled: " + provider);
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras)
-        {
-            Log.e(TAG, "onStatusChanged: " + status);
-        }
+    @Override public void onLocationChanged(Location location) {
+      Log.e(TAG, "LocationChanged: " + location.getLatitude());
+      Intent intent = new Intent("result");
+      intent.putExtra("lat", location.getLatitude());
+      sendBroadcast(intent);
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId)
-    {
-        super.onStartCommand(intent, flags, startId);
-        return START_NOT_STICKY;
+    @Override public void onConnected(Bundle bundle) {
+      startLocationUpdates();
     }
 
-    @Override
-    public void onCreate()
-    {
-        Log.i(TAG, "onCreate");
-        startForeground(12345678, getNotification());
-    }
-
-    @Override
-    public void onDestroy()
-    {
-        super.onDestroy();
-        if (mLocationManager != null) {
-            try {
-                mLocationManager.removeUpdates(mLocationListener);
-            } catch (Exception ex) {
-                Log.i(TAG, "fail to remove location listners, ignore", ex);
-            }
-        }
-    }
-
-    private void initializeLocationManager() {
-        if (mLocationManager == null) {
-            mLocationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
-        }
-    }
-
-    public void startTracking() {
-        initializeLocationManager();
-        mLocationListener = new LocationListener(LocationManager.GPS_PROVIDER);
-
-        try {
-            mLocationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, LOCATION_INTERVAL, LOCATION_DISTANCE, mLocationListener );
-
-        } catch (java.lang.SecurityException ex) {
-            // Log.i(TAG, "fail to request location update, ignore", ex);
-        } catch (IllegalArgumentException ex) {
-            // Log.d(TAG, "gps provider does not exist " + ex.getMessage());
-        }
+    @Override public void onConnectionSuspended(int i) {
 
     }
 
-    public void stopTracking() {
-        this.onDestroy();
+    @Override public void onConnectionFailed(ConnectionResult connectionResult) {
+
     }
+  }
 
-    private Notification getNotification() {
+  @Override public int onStartCommand(Intent intent, int flags, int startId) {
+    super.onStartCommand(intent, flags, startId);
+    return START_NOT_STICKY;
+  }
 
-        NotificationChannel channel = new NotificationChannel("channel_01", "My Channel", NotificationManager.IMPORTANCE_DEFAULT);
-
-        NotificationManager notificationManager = getSystemService(NotificationManager.class);
-        notificationManager.createNotificationChannel(channel);
-
-        Notification.Builder builder = new Notification.Builder(getApplicationContext(), "channel_01").setAutoCancel(true);
-        return builder.build();
+  @Override public void onCreate() {
+    Log.i(TAG, "onCreate");
+    createLocationRequest();
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      startForeground(12345678, getNotification());
     }
+  }
 
-    
-    public class LocationServiceBinder extends Binder {
-        public BackgroundService getService() {
-            return BackgroundService.this;
-        }
+  @Override public void onDestroy() {
+    super.onDestroy();
+    if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) mGoogleApiClient.disconnect();
+  }
+
+  public synchronized void buildGoogleApiClient() {
+    connectionCallbacks = connectionFailedListener = mLocationListener = new LocationListener();
+    mGoogleApiClient = new GoogleApiClient.Builder(this).addApi(LocationServices.API)
+        .addConnectionCallbacks(connectionCallbacks)
+        .addOnConnectionFailedListener(connectionFailedListener)
+        .build();
+    mGoogleApiClient.connect();
+  }
+
+  protected void startLocationUpdates() {
+    LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest,
+        mLocationListener);
+  }
+
+  protected void createLocationRequest() {
+    locationRequest = new LocationRequest();
+    locationRequest.setInterval(LOCATION_INTERVAL);
+    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+  }
+
+  public void stopTracking() {
+    this.onDestroy();
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.O) private Notification getNotification() {
+
+    NotificationChannel channel =
+        new NotificationChannel("channel_01", "My Channel", NotificationManager.IMPORTANCE_DEFAULT);
+
+    NotificationManager notificationManager = getSystemService(NotificationManager.class);
+    notificationManager.createNotificationChannel(channel);
+
+    Notification.Builder builder =
+        new Notification.Builder(getApplicationContext(), "channel_01").setAutoCancel(true);
+    return builder.build();
+  }
+
+  public class LocationServiceBinder extends Binder {
+    public BackgroundService getService() {
+      return BackgroundService.this;
     }
-
+  }
 }
