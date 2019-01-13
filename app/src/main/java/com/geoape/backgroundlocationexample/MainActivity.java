12e7d1986440a302ee949e7cfaa109a7ad5e8dc7
aparna603 <tanwar.rishabh0@gmail.com>
2019-01-13 23:49:41 +0530

    latest commit

--- app/src/main/java/com/geoape/backgroundlocationexample/MainActivity.java
+++ app/src/main/java/com/geoape/backgroundlocationexample/MainActivity.java
@@ -41,6 +41,9 @@ public class MainActivity extends AppCompatActivity {
 
   BroadcastReceiver receiver;
 
+
+
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
